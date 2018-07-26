a40e8ea6faa153b825340642bd39dc6722b19700
yishui <yishui@DESKTOP-DQH38FD>
2018-07-26 15:46:05 +0800

    日常工具初始化

---
+++ src/main/java/com/yishui/common/tool/format/LocalDateFormatter.java
@@ -0,0 +1,40 @@
+package com.yishui.common.tool.format;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import org.springframework.format.Formatter;
+
+/**
+ * 字符串与LocalDate转化类
+ * 
+ * @author yishui
+ * @date 2018年7月26日
+ * @Version 0.0.1
+ */
+public class LocalDateFormatter implements Formatter<LocalDate> {
+	private DateTimeFormatter formatter;
+	private String datePattern;
+
+	public LocalDateFormatter(String datePattern) {
+		this.datePattern = datePattern;
+		formatter = DateTimeFormatter.ofPattern(datePattern);
+	}
+
+	@Override
+	public LocalDate parse(String s, Locale locale) throws ParseException {
+		try {
+			return LocalDate.parse(s, DateTimeFormatter.ofPattern(datePattern));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public String print(LocalDate localDate, Locale locale) {
+		return localDate.format(formatter);
+	}
+}
---
+++ src/main/java/com/yishui/common/tool/format/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 格式化工具
+ * @author yishui
+ * @date 2018年7月26日
+ * @Version 0.0.1
+ */
+package com.yishui.common.tool.format;
