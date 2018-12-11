9a874d4e2cc8f31055b417a8dd0016ccb334af07
yishui <yishui@DESKTOP-DQH38FD>
2018-12-11 11:41:32 +0800

    增加空集合判断类

---
+++ src/main/java/com/yishui/common/tool/collections/EmptyUtil.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.yishui.common.tool.collections;
+
+import java.util.List;
+
+import com.github.pagehelper.PageInfo;
+
+/**
+ * 空集合判断类
+ * 
+ * @author yishui
+ * @date 2018年12月11日
+ * @Version 0.0.1
+ */
+public final class EmptyUtil {
+	/**
+	 * 判断是否为一个空的分页对象
+	 * 
+	 * @param pages
+	 *            分页对象
+	 * @return 如果是空返回为true，否则为false
+	 */
+	public static <T> boolean isEmpty(PageInfo<T> pages) {
+		if (pages == null) {
+			return true;
+		}
+		if (pages.getList() == null || pages.getList().size() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 判断是否为一个非空的分页对象
+	 * 
+	 * @param pages
+	 *            分页对象
+	 * @return 如果是空返回为false，否则为true
+	 */
+	public static <T> boolean notEmpty(PageInfo<T> pages) {
+		return !isEmpty(pages);
+	}
+
+	/**
+	 * 判断是否为空的列表
+	 * 
+	 * @param list
+	 *            列表
+	 * @return 如果为空返回为true，否则为false
+	 */
+	public static <T> boolean isEmpty(List<T> list) {
+		if (list == null) {
+			return true;
+		}
+		if (list.size() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 判断是否为空的列表
+	 * 
+	 * @param list
+	 *            列表
+	 * @return 如果为空返回为false，否则为true
+	 */
+	public static <T> boolean notEmpty(List<T> list) {
+		return !isEmpty(list);
+	}
+
+}
