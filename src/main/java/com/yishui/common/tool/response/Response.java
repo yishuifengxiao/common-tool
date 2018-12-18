34441b83568c52ad334126ac405a4c54eeee7141
yishui <yishui@DESKTOP-DQH38FD>
2018-12-18 20:24:42 +0800

    更新通用响应类的写法

--- src/main/java/com/yishui/common/tool/response/Response.java
+++ src/main/java/com/yishui/common/tool/response/Response.java
@@ -10,10 +10,10 @@ import org.springframework.http.HttpStatus;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.yishui.common.tool.utils.UID;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiParam;
 
 /**
  * 公有返回信息类<br/>
@@ -26,31 +26,8 @@ import io.swagger.annotations.ApiParam;
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @ApiModel(value = "通用返回实体类", description = "用于所有接口的通用返回数据")
-public class Response implements Serializable {
-	/**
-	 * 200响应码对应的默认信息
-	 */
-	private final static String OK = "请求成功";
-	/**
-	 * 400响应码对应的默认信息
-	 */
-	private final static String BAD_REQUEST = "请求参数有误";
-	/**
-	 * 401响应码对应的默认信息
-	 */
-	private final static String UNAUTHORIZED = "请求要求身份验证";
-	/**
-	 * 403响应码对应的默认信息
-	 */
-	private final static String FORBIDDEN = "无权访问此资源";
-	/**
-	 * 404响应码对应的默认信息
-	 */
-	private final static String NOT_FOUND = "访问的资源路径不存在";
-	/**
-	 * 500响应码对应的默认信息
-	 */
-	private final static String INTERNAL_SERVER_ERROR = "请求失败";
+public class Response<T> implements Serializable {
+
 	/**
 	 * 
 	 */
@@ -60,14 +37,6 @@ public class Response implements Serializable {
 	 */
 	@ApiModelProperty("请求ID,用于请求追踪 .无论调用接口成功与否,都会返回请求 ID,该序列号全局唯一且随机")
 	private String id;
-	/**
-	 * 返回消息中包含的token信息<br/>
-	 * 在以后的版本中将不在携带此信息，将只在获取token的接口里返回
-	 */
-	@ApiModelProperty(value = "返回消息的token", hidden = true)
-	@ApiParam(hidden = true)
-	@Deprecated
-	private String token;
 
 	/**
 	 * 请求的响应吗,这里借用HttpStatus作为状态标识
@@ -87,7 +56,7 @@ public class Response implements Serializable {
 	 * 返回的数据信息
 	 */
 	@ApiModelProperty("响应的数据信息，在基本基本信息无法满足时会出现此信息,一般情况下无此信息")
-	private Object data;
+	private T data;
 	/**
 	 * 返回数据的时间
 	 */
@@ -96,355 +65,38 @@ public class Response implements Serializable {
 	private Date date;
 
 	/**
-	 * 默认的请求成功时的返回信息(200响应码)
 	 * 
-	 * @return 请求成功的返回信息
 	 */
-	public static Response suc() {
-		return new Response(HttpStatus.OK.value(), OK);
-	}
+	public Response() {
 
-	/**
-	 * 默认的请求成功时的返回信息(200响应码)
-	 * 
-	 * @param msg
-	 *            请求成功时返回的基本信息
-	 * @return 请求成功的返回信息
-	 */
-	public static Response suc(String msg) {
-		return new Response(HttpStatus.OK.value(), msg);
-	}
-
-	/**
-	 * 默认的请求成功时的返回信息(200响应码)
-	 * 
-	 * @param msg
-	 *            请求成功时返回的基本信息
-	 * @param data
-	 *            请求成功时返回的数据信息
-	 * @return 请求成功的返回信息
-	 */
-	public static Response suc(String msg, Object data) {
-		return new Response(HttpStatus.OK.value(), msg, data);
-	}
-
-	/**
-	 * 默认的请求成功的返回信息(200响应码)
-	 * 
-	 * @param data
-	 *            请求成功时返回的数据信息
-	 * @return 请求成功的返回信息
-	 */
-	public static Response sucResult(Object data) {
-		return new Response(HttpStatus.OK.value(), OK, data);
-	}
-
-	/**
-	 * 默认的参数有误的返回信息(400响应码)
-	 * 
-	 * @return 参数有误时返回信息
-	 */
-	public static Response badParam() {
-		return new Response(HttpStatus.BAD_REQUEST.value(), BAD_REQUEST);
-	}
-
-	/**
-	 * 默认的参数有误的返回信息(400响应码)
-	 * 
-	 * @param msg
-	 *            参数有误时返回的基本信息
-	 * @return 参数有误时返回信息
-	 */
-	public static Response badParam(String msg) {
-		return new Response(HttpStatus.BAD_REQUEST.value(), msg);
-	}
-
-	/**
-	 * 默认的参数有误的返回信息(400响应码)
-	 * 
-	 * @param msg
-	 *            参数有误时返回的基本信息
-	 * @param data
-	 *            BAD_REQUEST
-	 * @return 默认的参数有误的返回信息
-	 */
-	public static Response badParam(String msg, Object data) {
-		return new Response(HttpStatus.BAD_REQUEST.value(), msg, data);
-	}
-
-	/**
-	 * 默认的参数有误的返回信息(400响应码)
-	 * 
-	 * @param data
-	 *            参数有误时返回的数据信息
-	 * @return 参数有误时返回信息
-	 */
-	public static Response badParamResult(Object data) {
-		return new Response(HttpStatus.BAD_REQUEST.value(), BAD_REQUEST, data);
-	}
-
-	/**
-	 * 资源未授权的返回信息(401响应码)
-	 * 
-	 * @return 资源未授权的返回信息
-	 */
-	public static Response unAuth() {
-		return new Response(HttpStatus.UNAUTHORIZED.value(), UNAUTHORIZED);
-	}
-
-	/**
-	 * 资源未授权的返回信息(401响应码)
-	 * 
-	 * @param msg
-	 *            资源未授权的返回信息的基本信息
-	 * @return 资源未授权的返回信息
-	 */
-	public static Response unAuth(String msg) {
-		return new Response(HttpStatus.UNAUTHORIZED.value(), msg);
-	}
-
-	/**
-	 * 资源未授权的返回信息(401响应码)
-	 * 
-	 * @param msg
-	 *            资源未授权的返回信息的基本信息
-	 * @param data
-	 *            资源未授权的返回信息的数据信息
-	 * @return 资源未授权的返回信息
-	 */
-	public static Response unAuth(String msg, Object data) {
-		return new Response(HttpStatus.UNAUTHORIZED.value(), msg, data);
-	}
-
-	/**
-	 * 资源未授权的返回信息(401响应码)
-	 * 
-	 * @param data
-	 *            资源未授权的返回信息的数据信息
-	 * @return 资源未授权的返回信息
-	 */
-	public static Response unAuthResult(Object data) {
-		return new Response(HttpStatus.UNAUTHORIZED.value(), UNAUTHORIZED, data);
-	}
-
-	/**
-	 * 资源不可用时的返回信息(403响应码)
-	 * 
-	 * @return 无权访问访问时的信息
-	 */
-	public static Response notAllow() {
-		return new Response(HttpStatus.FORBIDDEN.value(), FORBIDDEN);
-	}
-
-	/**
-	 * 资源不可用时的返回信息(403响应码)
-	 * 
-	 * @param msg
-	 *            参数有误时返回的基本信息
-	 * @return 无权访问访问时的信息
-	 */
-	public static Response notAllow(String msg) {
-		return new Response(HttpStatus.FORBIDDEN.value(), msg);
-	}
-
-	/**
-	 * 资源不可用时的返回信息(403响应码)
-	 * 
-	 * @param msg
-	 *            参数有误时返回的基本信息
-	 * @param data
-	 *            参数有误时返回的数据信息
-	 * @return 无权访问访问时的信息
-	 */
-	public static Response notAllow(String msg, Object data) {
-		return new Response(HttpStatus.FORBIDDEN.value(), msg, data);
-	}
-
-	/**
-	 * 资源不可用时的返回信息(403响应码)
-	 * 
-	 * @param data
-	 *            参数有误时返回的数据信息
-	 * @return 无权访问访问时的信息
-	 */
-	public static Response notAllowResult(Object data) {
-		return new Response(HttpStatus.FORBIDDEN.value(), FORBIDDEN, data);
-	}
-
-	/**
-	 * 默认的路径不存在时的返回信息(404响应码)
-	 * 
-	 * @return 默认的路径不存在时的返回信息
-	 */
-	public static Response notFoundt() {
-		return new Response(HttpStatus.NOT_FOUND.value(), NOT_FOUND);
-	}
-
-	/**
-	 * 默认的路径不存在时的返回信息(404响应码)
-	 * 
-	 * @param msg
-	 *            路径不存在时返回的基本信息
-	 * @return 默认的路径不存在时的返回信息
-	 */
-	public static Response notFoundt(String msg) {
-		return new Response(HttpStatus.NOT_FOUND.value(), msg);
-	}
-
-	/**
-	 * 默认的路径不存在时的返回信息(404响应码)
-	 * 
-	 * @param msg
-	 *            路径不存在时返回的基本信息
-	 * @param data
-	 *            路径不存在时返回的数据信息
-	 * @return 默认的路径不存在时的返回信息
-	 */
-	public static Response notFoundt(String msg, Object data) {
-		return new Response(HttpStatus.NOT_FOUND.value(), msg, data);
-	}
-
-	/**
-	 * 默认的路径不存在时的返回信息(404响应码)
-	 * 
-	 * @param msg
-	 *            路径不存在时返回的数据信息
-	 * @return 默认的路径不存在时的返回信息
-	 */
-	public static Response notFoundt(Object data) {
-		return new Response(HttpStatus.NOT_FOUND.value(), NOT_FOUND, data);
-	}
-
-	/**
-	 * 服务器内部异常时的返回信息(500响应码)
-	 * 
-	 * @return 服务器内部异常500时的返回信息
-	 */
-	public static Response error() {
-		return new Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), INTERNAL_SERVER_ERROR);
-	}
-
-	/**
-	 * 服务器内部异常时的返回信息(500响应码)
-	 * 
-	 * @param msg
-	 *            服务器内部异常时的返回信息的基本信息
-	 * @return 服务器内部异常500时的返回信息
-	 */
-	public static Response error(String msg) {
-		return new Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), msg);
-	}
-
-	/**
-	 * 服务器内部异常时的返回信息(500响应码)
-	 * 
-	 * @param msg
-	 *            服务器内部异常时的返回信息的基本信息
-	 * @param data
-	 *            服务器内部异常时的返回信息的数据信息
-	 * @return 服务器内部异常500时的返回信息
-	 */
-	public static Response error(String msg, Object data) {
-		return new Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), msg, data);
-	}
-
-	/**
-	 * 服务器内部异常时的返回信息(500响应码)
-	 * 
-	 * @param data
-	 *            服务器内部异常时的返回信息的数据信息
-	 * @return 服务器内部异常500时的返回信息
-	 */
-	public static Response errorResult(Object data) {
-		return new Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), INTERNAL_SERVER_ERROR, data);
-	}
-
-	/**
-	 * @return the id
-	 */
-	public String getId() {
-		return id;
 	}
 
 	/**
 	 * @param id
-	 *            the id to set
-	 */
-	public Response setId(String id) {
-		this.id = id;
-		return this;
-	}
-
-	/**
-	 * @return the token
-	 */
-	public String getToken() {
-		return token;
-	}
-
-
-	/**
-	 * @return the msg
-	 */
-	public String getMsg() {
-		return msg;
-	}
-
-	/**
+	 * @param code
 	 * @param msg
-	 *            the msg to set
-	 */
-	public Response setMsg(String msg) {
-		this.msg = msg;
-		return this;
-	}
-
-	/**
-	 * @return the data
-	 */
-	public Object getData() {
-		return data;
-	}
-
-	/**
 	 * @param data
-	 *            the data to set
+	 * @param date
 	 */
-	public Response setData(Object data) {
+	public Response(String id, int code, String msg, T data, Date date) {
+		this.id = id;
+		this.code = code;
+		this.msg = msg;
 		this.data = data;
-		return this;
-	}
-
-	/**
-	 * @return the date
-	 */
-	public Date getDate() {
-		return date;
-	}
-
-	/**
-	 * @return the code
-	 */
-	public int getCode() {
-		return code;
+		this.date = date;
 	}
 
 	/**
 	 * @param code
-	 *            the code to set
+	 * @param msg
+	 * @param data
 	 */
-	public Response setCode(int code) {
+	public Response(int code, String msg, T data) {
+		this.id = UID.uuid();
 		this.code = code;
-		return this;
-	}
-
-	/**
-	 * @param date
-	 *            the date to set
-	 */
-	public Response setDate(Date date) {
-		this.date = date;
-		return this;
+		this.msg = msg;
+		this.data = data;
+		this.date = new Date();
 	}
 
 	/**
@@ -452,52 +104,233 @@ public class Response implements Serializable {
 	 * @param msg
 	 */
 	public Response(int code, String msg) {
-		this.id = System.currentTimeMillis() + "";
-		this.msg = msg;
+		this.id = UID.uuid();
 		this.code = code;
+		this.msg = msg;
 		this.date = new Date();
 	}
 
 	/**
-	 * @param code
-	 * @param msg
-	 * @param data
+	 * 默认的请求成功时的返回信息(200响应码)
+	 * 
+	 * @return 请求成功的返回信息
 	 */
-	public Response(int code, String msg, Object data) {
-		this.id = System.currentTimeMillis() + "";
-		this.code = code;
-		this.msg = msg;
-		this.data = data;
-		this.date = new Date();
+	@SuppressWarnings("rawtypes")
+	public static Response suc() {
+		return new Response(HttpStatus.OK.value(), Msg.OK);
 	}
 
 	/**
-	 * @param id
-	 * @param code
+	 * 默认的请求成功时的返回信息(200响应码)
+	 * 
 	 * @param msg
-	 * @param data
-	 * @param date
+	 *            请求成功时返回的基本信息
+	 * @return 请求成功的返回信息
 	 */
-	public Response(String id, int code, String msg, Object data, Date date) {
+	@SuppressWarnings("rawtypes")
+	public static Response suc(String msg) {
+		return new Response(HttpStatus.OK.value(), msg);
+	}
+
+	/**
+	 * 默认的参数有误的返回信息(400响应码)
+	 * 
+	 * @return 参数有误时返回信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response badParam() {
+		return new Response(HttpStatus.BAD_REQUEST.value(), Msg.BAD_REQUEST);
+	}
+
+	/**
+	 * 默认的参数有误的返回信息(400响应码)
+	 * 
+	 * @param msg
+	 *            参数有误时返回的基本信息
+	 * @return 参数有误时返回信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response badParam(String msg) {
+		return new Response(HttpStatus.BAD_REQUEST.value(), msg);
+	}
+
+	/**
+	 * 资源未授权的返回信息(401响应码)
+	 * 
+	 * @return 资源未授权的返回信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response unAuth() {
+		return new Response(HttpStatus.UNAUTHORIZED.value(), Msg.UNAUTHORIZED);
+	}
+
+	/**
+	 * 资源未授权的返回信息(401响应码)
+	 * 
+	 * @param msg
+	 *            资源未授权的返回信息的基本信息
+	 * @return 资源未授权的返回信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response unAuth(String msg) {
+		return new Response(HttpStatus.UNAUTHORIZED.value(), msg);
+	}
+
+	/**
+	 * 资源不可用时的返回信息(403响应码)
+	 * 
+	 * @return 无权访问访问时的信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response notAllow() {
+		return new Response(HttpStatus.FORBIDDEN.value(), Msg.FORBIDDEN);
+	}
+
+	/**
+	 * 资源不可用时的返回信息(403响应码)
+	 * 
+	 * @param msg
+	 *            参数有误时返回的基本信息
+	 * @return 无权访问访问时的信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response notAllow(String msg) {
+		return new Response(HttpStatus.FORBIDDEN.value(), msg);
+	}
+
+	/**
+	 * 默认的路径不存在时的返回信息(404响应码)
+	 * 
+	 * @return 默认的路径不存在时的返回信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response notFoundt() {
+		return new Response(HttpStatus.NOT_FOUND.value(), Msg.NOT_FOUND);
+	}
+
+	/**
+	 * 服务器内部异常时的返回信息(500响应码)
+	 * 
+	 * @return 服务器内部异常500时的返回信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response error() {
+		return new Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), Msg.INTERNAL_SERVER_ERROR);
+	}
+
+	/**
+	 * 服务器内部异常时的返回信息(500响应码)
+	 * 
+	 * @param msg
+	 *            服务器内部异常时的返回信息的基本信息
+	 * @return 服务器内部异常500时的返回信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response error(String msg) {
+		return new Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), msg);
+	}
+
+	/**
+	 * 默认的路径不存在时的返回信息(404响应码)
+	 * 
+	 * @param msg
+	 *            路径不存在时返回的基本信息
+	 * @return 默认的路径不存在时的返回信息
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Response notFoundt(String msg) {
+		return new Response(HttpStatus.NOT_FOUND.value(), msg);
+	}
+
+
+
+
+
+	public String getId() {
+		return id;
+	}
+
+	public Response<T> setId(String id) {
 		this.id = id;
+		return this;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public Response<T> setCode(int code) {
 		this.code = code;
+		return this;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public Response<T> setMsg(String msg) {
 		this.msg = msg;
+		return this;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public Response<T> setData(T data) {
 		this.data = data;
+		return this;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public Response<T> setDate(Date date) {
 		this.date = date;
+		return this;
 	}
 
 
 
-	public Response() {
 
+
+	/**
+	 * 默认的返回信息
+	 * 
+	 * @author yishui
+	 * @date 2018年12月18日
+	 * @Version 0.0.1
+	 */
+	interface Msg {
+		/**
+		 * 200响应码对应的默认信息
+		 */
+		public final static String OK = "请求成功";
+		/**
+		 * 400响应码对应的默认信息
+		 */
+		public final static String BAD_REQUEST = "请求参数有误";
+		/**
+		 * 401响应码对应的默认信息
+		 */
+		public final static String UNAUTHORIZED = "请求要求身份验证";
+		/**
+		 * 403响应码对应的默认信息
+		 */
+		public final static String FORBIDDEN = "无权访问此资源";
+		/**
+		 * 404响应码对应的默认信息
+		 */
+		public final static String NOT_FOUND = "访问的资源路径不存在";
+		/**
+		 * 500响应码对应的默认信息
+		 */
+		public final static String INTERNAL_SERVER_ERROR = "请求失败";
 	}
 
-	
-	@Override
-	public String toString() {
-		return "Response [id=" + id + ", token=" + token + ", code=" + code + ", msg=" + msg + ", data=" + data
-				+ ", date=" + date + "]";
-	}
+
+
 
 
 	@Override
@@ -512,7 +345,7 @@ public class Response implements Serializable {
 		return result;
 	}
 
-	
+	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -529,8 +362,9 @@ public class Response implements Serializable {
 			return false;
 		}
 		if (data == null) {
-			if (other.data != null){
-				return false;}
+			if (other.data != null) {
+				return false;
+			}
 		} else if (!data.equals(other.data)) {
 			return false;
 		}
@@ -557,5 +391,13 @@ public class Response implements Serializable {
 		}
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		return "Response [id=" + id + ", code=" + code + ", msg=" + msg + ", data=" + data + ", date=" + date + "]";
+	}
+	
+	
+	
 
 }
