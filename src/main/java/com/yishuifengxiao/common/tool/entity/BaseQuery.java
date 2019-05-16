9d29a8117a87d53e58c3c4bbe5cd590cae4e67b7
yishui <yishui@DESKTOP-DQH38FD>
2019-05-16 17:03:32 +0800

    优化

--- src/main/java/com/yishuifengxiao/common/tool/entity/BaseQuery.java
+++ src/main/java/com/yishuifengxiao/common/tool/entity/BaseQuery.java
@@ -68,4 +68,42 @@ public class BaseQuery implements Serializable {
 
 	}
 
+	@Override
+	public String toString() {
+		return "BaseQuery [pageSize=" + pageSize + ", pageNum=" + pageNum + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((pageNum == null) ? 0 : pageNum.hashCode());
+		result = prime * result + ((pageSize == null) ? 0 : pageSize.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BaseQuery other = (BaseQuery) obj;
+		if (pageNum == null) {
+			if (other.pageNum != null)
+				return false;
+		} else if (!pageNum.equals(other.pageNum))
+			return false;
+		if (pageSize == null) {
+			if (other.pageSize != null)
+				return false;
+		} else if (!pageSize.equals(other.pageSize))
+			return false;
+		return true;
+	}
+	
+	
+
 }
