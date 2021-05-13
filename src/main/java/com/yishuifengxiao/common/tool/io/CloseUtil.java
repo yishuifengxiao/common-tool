4574b97f21a59599c3f8c58638fdbbf35004467f
yishuifengxiao <zhiyubujian@163.com>
2021-05-13 09:54:22 +0800

    优化接口

--- src/main/java/com/yishuifengxiao/common/tool/io/CloseUtil.java
+++ src/main/java/com/yishuifengxiao/common/tool/io/CloseUtil.java
@@ -28,7 +28,7 @@ public class CloseUtil {
 	 * 
 	 * @param closeables 需要关闭的IO流
 	 */
-	public static synchronized void close(Closeable... closeables) {
+	public static void close(Closeable... closeables) {
 		if (null == closeables) {
 			return;
 		}
@@ -50,7 +50,7 @@ public class CloseUtil {
 	 * 
 	 * @param inputStream 输入流
 	 */
-	public static synchronized void close(InputStream inputStream) {
+	public static void close(InputStream inputStream) {
 		if (null != inputStream) {
 			try {
 				inputStream.close();
@@ -66,7 +66,7 @@ public class CloseUtil {
 	 * 
 	 * @param outputStream 输出流
 	 */
-	public synchronized static void close(OutputStream outputStream) {
+	public static void close(OutputStream outputStream) {
 		if (null != outputStream) {
 			try {
 				outputStream.close();
@@ -82,7 +82,7 @@ public class CloseUtil {
 	 * 
 	 * @param reader 字符流读取器
 	 */
-	public synchronized static void close(Reader reader) {
+	public static void close(Reader reader) {
 		if (null != reader) {
 			try {
 				reader.close();
@@ -98,7 +98,7 @@ public class CloseUtil {
 	 * 
 	 * @param writer 字符流写入器
 	 */
-	public synchronized static void close(Writer writer) {
+	public static void close(Writer writer) {
 		if (null != writer) {
 			try {
 				writer.close();
@@ -109,19 +109,4 @@ public class CloseUtil {
 		}
 	}
 
-	/**
-	 * 关闭一个可以关闭的数据的源或目的地
-	 * 
-	 * @param closeable 可以关闭的数据的源或目的地
-	 */
-	public synchronized static void close(Closeable closeable) {
-		if (null != closeable) {
-			try {
-				closeable.close();
-				closeable = null;
-			} catch (Exception e) {
-				log.debug("【易水工具】关闭可以关闭的数据的源或目的地时出现问题，出现问题的原因为 {}", e.getMessage());
-			}
-		}
-	}
 }
